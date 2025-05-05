63974ba75f6fd7b8ce675cb9843790737aa9ca8e
Magno Fernando Brixner Biét <magno.biet@gmail.com>
2025-05-05 13:50:01 -0300

    chore: update main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,48 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
-
+import Layout from '@theme/Layout';
+import clsx from 'clsx';
+import type { ReactNode } from 'react';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
+
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
+
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
+
       </div>
-    </header>
+    </header >
   );
 }
 
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`./index`}
+      description="Links, tendências e ferramentas para devs front-end, todo mês!">
+
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
+
+      <main className={styles.buttons}>
+        <Link
+          className="button button--secondary button--lg"
+          to="/archive">
+          Ver edições anteriores
+        </Link>
+
+        <Link
+          className="button button--primary button--lg"
+          to="https://www.linkedin.com/build-relation/newsletter-follow?entityUrn=7289953302857801730">
+          Assinar no LinkedIn
+        </Link>
       </main>
     </Layout>
   );
